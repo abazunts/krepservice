c69579941327b0cb1b21aa7eb62f12bf31b809ba
Artem <bazunc@gmail.com>
2020-03-19 00:20:14 +0500

    Updates

---
+++ precache-manifest.549dd5db776e0e47f98013ba38cf96c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e66c04fecc8924e9c772d24d622157",
+    "url": "/krepservice/index.html"
+  },
+  {
+    "revision": "2bbca17be32a439195ef",
+    "url": "/krepservice/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "f359381593b60fb87867",
+    "url": "/krepservice/static/css/main.1e4fea12.chunk.css"
+  },
+  {
+    "revision": "2bbca17be32a439195ef",
+    "url": "/krepservice/static/js/2.7d69062b.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/krepservice/static/js/2.7d69062b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f359381593b60fb87867",
+    "url": "/krepservice/static/js/main.c5def64a.chunk.js"
+  },
+  {
+    "revision": "742dcf948bc05bec8d41",
+    "url": "/krepservice/static/js/runtime-main.734c0928.js"
+  },
+  {
+    "revision": "e3c4feb33e42d2b3c1db3e78b4fd27f9",
+    "url": "/krepservice/static/media/krep.e3c4feb3.png"
+  },
+  {
+    "revision": "7f2ac77cdc169000a02acba7d4f4021d",
+    "url": "/krepservice/static/media/krep1.7f2ac77c.png"
+  },
+  {
+    "revision": "91cc6532f6a91ceae108db11dcfac598",
+    "url": "/krepservice/static/media/logo-white.91cc6532.png"
+  },
+  {
+    "revision": "0d6ce701d384958403804b814207a431",
+    "url": "/krepservice/static/media/whatsapp.0d6ce701.png"
+  }
+]);
