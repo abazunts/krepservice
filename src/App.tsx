04129f586f70107249f88272fe12fa2878a77586
Artem Bazunts <bazunc@gmail.com>
2020-03-18 17:04:09 +0500

    refactor header

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "./ui/header/Header";
 import {Route} from 'react-router-dom';
 import {createStyles, WithStyles, withStyles} from "@material-ui/core";
+import ProductCard from "./ui/product-card/ProductCard";
 
 const styles = createStyles({
     root: {
         display: 'flex',
         flexDirection: 'column',
-        fontFamily: 'Verdana'
+        fontFamily: 'Verdana',
+        alignItems: 'center',
     },
     headerWrapper: {
         position: 'fixed',
@@ -18,9 +20,15 @@ const styles = createStyles({
         boxShadow: ' 0px 10px 13px -12px rgba(57,63,75,1)'
     },
     wrapperContent: {
+        display: 'flex',
+        flexWrap:'wrap',
         position: 'relative',
+        width: '60%',
         marginTop: 200,
-        border: '1px solid red'
+        border: '1px solid red',
+        ['@media (max-width:440px)']: {
+            width: '98%',
+        }
     },
 });
 
@@ -35,7 +43,7 @@ const App: FC<IProps> = ({classes}) => {
                 <Header/>
             </div>
             <div className={classes.wrapperContent}>
-                <Route exact path={'/'} render={() => <div>Главная</div>}/>
+                <Route exact path={'/'} render={() => <div><ProductCard/></div>}/>
                 <Route  path={'/about'} render={() => <div>О компании</div>}/>
                 <Route  path={'/price'} render={() => <div>Продукция</div>}/>
                 <Route  path={'/contacts'} render={() => <div>Контакты</div>}/>
--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -3,17 +3,23 @@ import {createStyles, WithStyles, withStyles} from "@material-ui/core";
 import MainMenu from "../main-menu/MainMenu";
 import PhoneIcon from '@material-ui/icons/Phone';
 import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import logo from '../../assets/img/logo-white.png'
+import x from '../../assets/img/x-circle.png'
+import krep from '../../assets/img/krep.png'
+import krep1 from '../../assets/img/krep1.png'
+import {NavLink} from "react-router-dom";
 
 const styles = createStyles({
     headerCenter: {
         borderTop: '5px solid #f94836',
         height: '10vh',
         minHeight: 94,
-        background: 'white',
+        background: '#f94836',
+        zIndex: 999,
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        flexWrap: 'wrap',
+        flexWrap: 'nowrap',
         "&:before": {
             content: "''",
             right: 0,
@@ -24,17 +30,30 @@ const styles = createStyles({
             position: 'fixed',
             width: '65%',
         },
-        ['@media (max-width:980px)']: {
-            justifyContent: 'center'
+        ['@media (max-width:1457px)']: {
+            justifyContent: 'center',
+            "&:before": {
+                width: '55%',
+            },
+        },
+        ['@media (max-width:768px)']: {
+            justifyContent: 'center',
+            "&:before": {
+                width: '0%',
+                borderLeft: '0px solid transparent',
+            },
         }
     },
     headerBottom: {
+        position: 'relative',
         display: 'flex',
         justifyContent: 'flex-end',
         alignItems: 'stretch',
         background: '#393f4b',
         height: '8vh',
-        minHeight: 70
+        minHeight: 70,
+        zIndex: 9999,
+        boxShadow: '0px -2px 10px 1px rgba(0,0,0,0.80)',
     },
     menuWrapper: {
         marginRight: '5%',
@@ -60,9 +79,9 @@ const styles = createStyles({
         justifyContent: 'center',
         alignItems: 'center',
         zIndex: 999,
-        minWidth: 400,
+        minWidth: 450,
         marginLeft: '15%',
-        ['@media (max-width:980px)']: {
+        ['@media (max-width:768px)']: {
             display: 'none'
         }
     },
@@ -71,15 +90,81 @@ const styles = createStyles({
         justifyContent: 'flex-start',
         alignItems: 'center',
         zIndex: 999,
-        minWidth: 400,
+        minWidth: 450,
         marginLeft: '10%',
     },
     wrapperLogo: {
         zIndex: 999,
-        marginLeft: '15%',
+        marginLeft: '10%',
         ['@media (max-width:980px)']: {
-            marginLeft: 0
+            marginLeft: 10
         }
+    },
+    logo: {
+        cursor: 'pointer',
+        width: 300,
+    },
+    x: {
+        width: 15,
+        position: 'absolute',
+        left: 10,
+        top: 10
+    },
+    x2: {
+        width: 15,
+        position: 'absolute',
+        left: 10,
+        top: 65
+    },
+    x3: {
+        width: 15,
+        position: 'absolute',
+        left: '34%',
+        top: 65,
+        ['@media (max-width:1457px)']: {
+            left: '44%',
+        },
+        ['@media (max-width:768px)']: {
+            left: 'auto',
+            right: 10
+        }
+    },
+    x4: {
+        width: 15,
+        position: 'absolute',
+        left: '36%',
+        top: 10,
+        ['@media (max-width:1457px)']: {
+            left: '46.5%',
+        },
+        ['@media (max-width:1050px)']: {
+            left: '47.5%',
+        },
+        ['@media (max-width:768px)']: {
+            left: 'auto',
+            right: 10
+        }
+    },
+    krep1: {
+        width: 400,
+        opacity: '0.1',
+        position: 'absolute',
+        right: 0,
+        top: 5,
+        ['@media (max-width:1457px)']: {
+            display: 'none',
+        },
+    },
+    krep: {
+        width: 400,
+        opacity: '0.1',
+        position: 'absolute',
+        right: 400,
+        top: 5,
+        transform: 'rotate(-8deg)',
+        ['@media (max-width:1457px)']: {
+            display: 'none',
+        },
     }
 });
 
@@ -88,21 +173,25 @@ interface IProps extends WithStyles<typeof styles> {
 }
 
 const Header: FC<IProps> = ({classes}) => {
-
     return <div>
         <div className={classes.headerCenter}>
+            <img src={x} className={classes.x}/>
+            <img src={x} className={classes.x2}/>
+            <img src={x} className={classes.x3}/>
+            <img src={x} className={classes.x4}/>
             <div className={classes.wrapperLogo}>
-                <img src={'http://krepmaster.kz/thumb/2/ioFwbRQHVDdnwWayrO8mVg/282r48/d/logo.png'}/>
+                <NavLink to={'/'}><img src={logo} className={classes.logo}/></NavLink>
             </div>
             <div className={classes.wrapperPhone}>
                 <div className={classes.information}>
-                    <PhoneIcon style={{color: 'red', marginRight: 10}}/><span className={classes.phone}>8 771 177 32 42 / 8 7122 28-64-75</span>
+                    <PhoneIcon style={{color: '#f94836', marginRight: 10}}/><span className={classes.phone}>8 702 792 70 02 / 8 701 022 60 02 / 29 84 75 / 29 04 64</span>
                 </div>
                 <div className={classes.information}>
-                    <QueryBuilderIcon style={{color: 'red', marginRight: 10}}/><span className={classes.date}>Пн–Пт 09:00–18:00, Сб 09:00–16:00</span>
+                    <QueryBuilderIcon style={{color: '#f94836', marginRight: 10}}/><span className={classes.date}>Пн–Пт 09:00–18:00, Сб 10:00–16:00</span>
                 </div>
             </div>
-
+            <img src={krep1} className={classes.krep1}/>
+            <img src={krep} className={classes.krep}/>
         </div>
         <div className={classes.headerBottom}>
             <div className={classes.menuWrapper}>
